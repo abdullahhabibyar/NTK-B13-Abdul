cf94670a0101abced410ea0548acba2165c9b957
ahabi <ahabi@Abdullah.fios-router.home>
2025-02-13 14:34:19 -0500

    first HW Code implemented

--- src/test/java/demoHWS/Exersice.java
+++ src/test/java/demoHWS/Exersice.java
@@ -1,11 +1,50 @@
 package demoHWS;
 
-public class Exersice {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import utils.Hooks;
+
+public class Exersice extends Hooks{
 	
-	public static void main (String args[]) {
+	public static void main (String args[]) throws InterruptedException {
 		
 		
-		int b = 10;
+	
+		
+		
+		setUp("edge");
+		
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+		
+		WebElement usernameField = driver.findElement(By.name("username"));	
+		
+		usernameField.sendKeys("admin");
+		Thread.sleep(2000);
+		
+		WebElement passwordField = driver.findElement(By.name("password"));	
+		
+		passwordField.sendKeys("admin123");
+		Thread.sleep(2000);
+		
+		WebElement loginButton = driver.findElement(By.xpath("/html/body/div/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+		 
+		loginButton.click();
+		Thread.sleep(2000);
+		
+		WebElement HearderField = driver.findElement(By.className("oxd-topbar-header-breadcrumb")) 	;	
+	    HearderField.click();
+	    Thread.sleep(2000);
+
+		
+		tearDown();
 	}
 
-}
+	
+	
+		
+	}
+
+
